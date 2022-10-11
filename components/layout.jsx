e197013609cb58cfb81e10d7ff39cd6e10c444db
AlsolwiAisha <aisha.alsolwi@gmail.com>
2022-10-11 22:24:45 +0300

    Create layout.jsx

---
+++ components/layout.jsx
@@ -0,0 +1,16 @@
+import React, { Children } from 'react'
+import Footer from './footer'
+import Navbar from './navbar'
+
+const Layout = ({children}) => {
+  return (
+    <div>
+      <Navbar/>
+        {children}
+      <Footer/>
+    </div>
+  )
+}
+
+export default Layout
+
